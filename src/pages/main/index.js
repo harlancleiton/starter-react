2f0555469c95c41ac7b92420a16b03783ff7932c
Harlan Cleiton <harlancleiton@gmail.com>
2019-10-14 08:16:48 -0300

    Finished

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,33 +1,75 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import api from '../../services/api';
-import { ProductDescription, ProductList, ProductItem, Button } from './styles';
+import {
+  Btn,
+  BtnGroup,
+  ProductDescription,
+  ProductList,
+  ProductItem,
+  Button
+} from './styles';
 
 export default class Main extends Component {
   state = {
-    products: []
+    products: [],
+    productInfo: {},
+    page: 1
   };
 
   componentDidMount() {
     this.loadProducts();
   }
 
-  loadProducts = async () => {
-    const response = await api.get('/products');
+  loadProducts = async (page = 1) => {
+    const response = await api.get(`/products?page=${page}`);
 
-    this.setState({ products: response.data.docs });
+    const { docs, ...productInfo } = response.data;
+
+    this.setState({ products: docs, productInfo, page });
+  };
+
+  handlePrevPage = () => {
+    const { page } = this.state;
+    if (page === 1) return;
+
+    const pageNumber = page - 1;
+    this.loadProducts(pageNumber);
+  };
+
+  handleNextPage = () => {
+    const { page, productInfo } = this.state;
+    if (page === productInfo.pages) return;
+
+    const pageNumber = page + 1;
+    this.loadProducts(pageNumber);
   };
 
   render() {
-    const { products } = this.state;
+    const { products, productInfo, page } = this.state;
     return (
       <ProductList>
         {products.map(product => (
           <ProductItem key={product._id}>
             <strong>{product.title}</strong>
             <ProductDescription>{product.description}</ProductDescription>
-            <Button href="">Acessar</Button>
+            {/* <Button href="">Acessar</Button> */}
+            <Link to={`/products/${product._id}`}>
+              <Button>Acessar</Button>
+            </Link>
           </ProductItem>
         ))}
+        <BtnGroup>
+          <Btn disabled={page === 1} onClick={this.handlePrevPage}>
+            Anterior
+          </Btn>
+          <Btn
+            disabled={page === productInfo.page}
+            onClick={this.handleNextPage}
+          >
+            Próximo
+          </Btn>
+        </BtnGroup>
       </ProductList>
     );
   }
--- src/pages/main/styles.js
+++ src/pages/main/styles.js
@@ -40,3 +40,23 @@ export const Button = styled.a`
     color: #fff;
   }
 `;
+
+export const BtnGroup = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 20px;
+`;
+
+export const Btn = styled.button`
+  padding: 10px;
+  border-radius: 5px;
+  border: 0;
+  background: #da552f;
+  color: #fff;
+  font-size: 16px;
+  font-weight: bold;
+  &:hover {
+    opacity: 0.7;
+    cursor: pointer;
+  }
+`;
